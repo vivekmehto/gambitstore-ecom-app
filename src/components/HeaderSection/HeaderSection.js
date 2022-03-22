4b844574d0d0a6ae1bcecc9d386a2c19b3fa9443
vivekmehto <vivekmehto.webdev@gmail.com>
2022-03-22 15:07:13 +0530

    ADD: home page

--- src/components/HeaderSection/HeaderSection.js
+++ src/components/HeaderSection/HeaderSection.js
@@ -1,48 +1,34 @@
 import { Link } from "react-router-dom";
+import { useCategoryData } from "../../utils";
 import "./style.css";
 
-// TODO: to make category map and background img
 const HeaderSection = () => {
+  const categories = useCategoryData();
   return (
     <>
       <aside className="category-list">
-        <div className="category-topic">
-          <Link to="/products">
-            <i className="fa fa-regular fa-gift"></i>
-            <p>Top Offers</p>
-          </Link>
-        </div>
-        <div className="category-topic">
-          <Link to="/products">
-            <i className="fa fa-regular fa-book"></i>
-            <p>Books</p>
-          </Link>
-        </div>
-        <div className="category-topic">
-          <Link to="/products">
-            <i className="fa fa-regular fa-chess"></i>
-            <p>Chess Set</p>
-          </Link>
-        </div>
-        <div className="category-topic">
-          <Link to="/products">
-            <i className="fa fa-regular fa-book-open"></i>
-            <p>Note Books</p>
-          </Link>
-        </div>
-        <div className="category-topic">
-          <Link to="/products">
-            <i className="fa fa-regular fa-pen"></i>
-            <p>Pens</p>
-          </Link>
-        </div>
+        {categories.map(({ _id, categoryName, imgUrl }) => (
+          <div className="category-topic" key={_id}>
+            <Link to="/products">
+              <img
+                className="category-img img-responsive"
+                src={imgUrl}
+                alt={categoryName}
+              />
+              <p className="m-05 text-up fw-b">{categoryName}</p>
+            </Link>
+          </div>
+        ))}
       </aside>
 
       <main className="main-store">
         <div className="hero-container">
           <div className="hero-head">
             <h1 className="head-1">Exclusive Chess Products</h1>
-            <Link to="/products"> SHOP NOW </Link>
+            <Link to="/products" className="btn btn-lg">
+              {" "}
+              SHOP NOW{" "}
+            </Link>
           </div>
         </div>
       </main>
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,11 @@
-import { Navbar, HeaderSection } from "../../components";
+import { Navbar, HeaderSection, Footer } from "../../components";
 import "./style.css";
 const Home = () => {
   return (
     <div>
       <Navbar />
       <HeaderSection />
+      <Footer />
     </div>
   );
 };
